bcbb9b673aa2243e8d17d6895ec435ea17f1281d
Cory <cfowler1219@gmail.com>
2025-02-18 21:43:00 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CS430_FowlerW1Assignment',
+  webDir: 'www'
+};
+
+export default config;
